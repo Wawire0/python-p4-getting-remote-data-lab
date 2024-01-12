e7fd03b501d973aafe3da7da7c7c343bd2f30e65
Sandra <listerbernice@gmail.com>
2024-01-13 01:19:59 +0300

    Completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,23 @@
 import requests
 import json
 
-class GetRequester:
+class GetPrograms:
 
-    def __init__(self, url):
-        self.url = url
+    @staticmethod
+    def get_programs():
+        URL = "http://data.cityofnewyork.us/resource/uvks-tn5n.json"
 
-    def get_response_body(self):
-        pass
+        try:
+            response = requests.get(URL)
+            response.raise_for_status() # Raises a HTTPError if the response was an HTTP error (4xx or 5xx).
 
-    def load_json(self):
-        pass
+            return response.json()
+        except requests.exceptions.HTTPError as http_err:
+            print(f'HTTP error occurred: {http_err}')
+        except requests.exceptions.RequestException as err:
+            print(f'Error occurred: {err}')
+        except Exception as e:
+            print(f'An error occurred: {e}')
+
+programs = GetPrograms.get_programs()
+print(programs)
